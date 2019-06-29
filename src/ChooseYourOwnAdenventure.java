9998cbbe91029ec45e9a020a45bd83883dc72863
League <league@Mac-mini.local>
2019-06-29 11:22:14 -0700

    I'm almost done whith chosse your own adventrue

--- src/ChooseYourOwnAdenventure.java
+++ src/ChooseYourOwnAdenventure.java
@@ -5,11 +5,11 @@ public class ChooseYourOwnAdenventure {
 	public static void main(String[] args) {
 
 		String door = JOptionPane.showInputDialog(
-				"there are two doors both are black whith gold writing on the left door it says sword and the door on the right says sheild which door are you going thougrh.");
+				"there are two doors both are black whith gold writing on the left door it says sword and the door on the right says shield which door are you going thougrh.");
 		if ("left".equals(door)) {
 			String sword = JOptionPane.showInputDialog("you found a sword you will pick it up.");
 			if ("pick it up".equals(sword))
-				JOptionPane.showInputDialog(
+				JOptionPane.showMessageDialog(
 						"you pick up the sword and you see a swarm of monsters and run you are called a coward for the rest of your life.");
 		}
 
